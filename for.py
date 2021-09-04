0e060f07e6b44e58c442b7ae30e729d86878be17
kameoman <spiderman.935963@gmail.com>
2021-09-04 09:02:08 +0900

    for

--- for.py
+++ for.py
@@ -1,11 +1,27 @@
 # 3の倍数のカウント
-n = int(input())
-a = input().split()
+# n = int(input())
+# a = input().split()
 
-ans = 0
+# ans = 0
+
+# for i in range(n):
+#     if int(a[i]) % 3 == 0:
+#         ans += 1
+
+# print(i)
+
+#フラグ管理
+
+n = int(input())
+
+flag = False
 
 for i in range(n):
-    if int(a[i]) % 3 == 0:
-        ans += 1
+    a = int(input())
+    if a == 7:
+        flag = True
 
-print(i)
+if flag:
+    print("YES")
+else:
+    print("NO")
